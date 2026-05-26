ac2480f6b83b60f449064b24a200da64ed2350bc
Yuyuwayu <simpersherta@gmail.com>
2026-05-26 09:02:10 +0700

    Update detection paths for Robloflow dataset

--- scripts/detect_image.py
+++ scripts/detect_image.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO("runs/detect/train/weights/best.pt")
+model = YOLO(r"runs\detect\train18\weights\best.pt")
 
-img = cv2.imread("train/images/0a8d9a25-dd66-4e24-9acf-f1dc17f31942_jpg.rf.11775faa9e2dfb687a4bfce3fde58c9e.jpg")
+img = cv2.imread(r"dataset\Robloflow\images\train\IMG_35mm160_00173_jpg.rf.xWVd1SuKLyXafg23lGPy.jpg")
 img = cv2.resize(img, (640, 480))
 
 results = model(img)
